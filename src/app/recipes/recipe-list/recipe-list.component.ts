b24d7fdf7b0089fd29c44d65d544fb4b47f92708
jayden owens <jaydenowens23@gmail.com>
2022-11-17 22:46:51 -0700

    course push

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipe: Recipe[] = [
+    new Recipe('Test Recipe', 'this is just a test', 'https://cdn.stocksnap.io/img-thumbs/960w/food-recipe_G8QICMKLUV.jpg')
+
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
